139b4013784136a26f921ceb3aed20a5343b3cc1
Philipp Bisson <pb@25th-floor.com>
2018-10-01 11:37:59 +0200

    Refactor Rect Calculation

--- src/assets/sprite.rs
+++ src/assets/sprite.rs
@@ -1,49 +1,24 @@
-use assets::tileparser::Tileset;
+use assets::tileparser::{Properties, Tile, Tileset};
 use ggez::graphics::Rect;
 use std::collections::HashMap;
 
+// REFACTOR !!!
 #[derive(Debug, Clone)]
 pub struct Sprite {
     pub sprite_id: i32,
     pub src: Rect,
+    pub properties: Properties,
 }
 
 pub type ManagedSprite = HashMap<i32, Sprite>;
 
-fn get_coords_of_tile_id(w: i32, h: i32, id: i32) -> (i32, i32) {
-    let row = (id / w) as i32;
-    let col = id % w;
-    let x = row * w;
-    let y = col * h;
-    (x, y)
-}
-
-fn get_rect_src(x: i32, y: i32, image_height: i32, image_width: i32) -> Rect {
-    // let a = Rect::new(y as f32, x as f32, 16 as f32, 16 as f32);
-    // println!("{} {}", x, y);
-    let a = Rect::new(
-        y as f32 / (image_height) as f32,
-        x as f32 / (image_width) as f32,
-        16.0 / (image_height) as f32,
-        16.0 / (image_width) as f32,
-    );
-    // println!("{:?}", a);
-    a
-}
-
 pub fn create_sprite_with_index(tileset: Tileset, firstgid: i32, sprites: &mut ManagedSprite) {
-    let tw = tileset.tilewidth;
-    let th = tileset.tileheight;
-    let iw = tileset.imagewidth;
-    let ih = tileset.imagewidth;
-
-    if let Some(tiles) = tileset.tiles {
+    if let Some(tiles) = tileset.clone().tiles {
         for tile in tiles.into_iter() {
-            let (x, y) = get_coords_of_tile_id(tw, th, tile.id);
-            let src = get_rect_src(x, y, ih, iw);
-
+            let src = calc_rect(&tileset, &tile);
             let e_tile = Sprite {
                 sprite_id: tile.id,
+                properties: tile.properties,
                 src,
             };
             sprites.entry(tile.id + &firstgid).or_insert(e_tile);
@@ -51,4 +26,38 @@ pub fn create_sprite_with_index(tileset: Tileset, firstgid: i32, sprites: &mut M
     } else {
         debug!("\ntileset: \n{:?}", tileset);
     }
+}
+
+fn calc_rect(tileset: &Tileset, tile: &Tile) -> Rect {
+    let iw = tileset.imagewidth as f32;
+    let ih = tileset.imageheight as f32;
+
+    let c = tileset.columns as f32;
+    let w = tileset.tilewidth as f32;
+    let h = tileset.tileheight as f32;
+    let tile_id = tile.id as f32;
+
+    let rw = w / iw;
+    let rh = h / ih;
+
+    let x = (tile_id % c * w) / iw;
+    let y = ((tile_id / c) as i32 * 16) as f32 / ih;
+
+    Rect::new(x, y, rw, rh)
+}
+
+#[test]
+fn name() {
+    use assets::tileparser::parse_tileset;
+    let path = "resources/ashlands_tileset.json";
+    let tileset = parse_tileset(path.to_string()).unwrap_or_else(|_| {
+        panic!("Didn't find file {}");
+    });
+    if let Some(tiles) = tileset.clone().tiles {
+        let tile = tiles.get(0).unwrap();
+        let a = calc_rect(&tileset, &tile);
+        println!("--> {:#?}", a);
+    }
+
+    assert_eq!(1, 1);
 }
